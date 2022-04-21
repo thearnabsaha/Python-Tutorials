e78fe418b0d63c93e51e122f1d82feff69b228b3
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-21 18:13:51 +0530

    s1

--- chapter-1.py
+++ chapter-1.py
@@ -11,3 +11,11 @@ print("hello world \\ arnab")
 
 
 print("hello world \\n arnab")
+print("hello world \\\" \\\' arnab")
+
+
+
+print("this is \\\\ double backslash")
+print("these are /\\/\\/\\/\\/\\ mountains")
+print("he is \t awsome")
+print("\\\" \\n \\t \\'")
