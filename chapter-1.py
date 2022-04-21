9a563255b09ca8c24d2c0f91893766794d2aae67
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-21 18:02:28 +0530

    s1

---
+++ chapter-1.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("Hello World's world")
+print('hello world')
+print('hello world "is best"')
+
+print("hello world \" arnab \" ")
+print('hello world \' arnab \' ')
+print("hello world \n arnab")
+print("hello world \t arnab")
+print("hello world \\ arnab")
