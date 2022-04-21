4f21a02ae16a5af7fc93b507617667a77e3f8bc2
thearnabsaha <thearnabsaha201@gmail.com>
2022-04-21 19:26:43 +0530

    s1

--- chapter-1.py
+++ chapter-1.py
@@ -19,3 +19,13 @@ print("this is \\\\ double backslash")
 print("these are /\\/\\/\\/\\/\\ mountains")
 print("he is \t awsome")
 print("\\\" \\n \\t \\'")
+print("\U0001F602")
+
+print(2+3) #add
+print(2-3) #subtract
+print(2*3) #multiply
+print(2/3) #floting division
+print(2%3) #modulo for remainder
+print(2//3) #interger division
+print(2**3) #exponent
+print(round(5/3,4))  #round of till 4 integers
