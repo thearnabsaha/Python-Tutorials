0e1ac091331a2bd802d76efe9beeae933434b6f6
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-05 02:40:48 +0530

    s2

--- chapter-2.py
+++ chapter-2.py
@@ -1,8 +1,8 @@
 first_name = "arnab"
 last_name="saha"
 full_name= first_name+""+last_name
-print(full_name)
-print(full_name+3)
-print(full_name+"3")
-print(full_name+str(3))
-print(full_name*3)
+print(full_name)      #No Error
+# print(full_name+3)  // error  //becuse types are different here
+print(full_name+"3")      #No Error      
+print(full_name+str(3))      #No Error   //make 3 a str
+print(full_name*3)      #No Error       //multiplies name for 3 times
