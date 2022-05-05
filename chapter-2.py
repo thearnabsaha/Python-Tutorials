7c70734f21c75810b2eeb78cfca158bd9a5aee8b
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-05 21:05:40 +0530

    s2

--- chapter-2.py
+++ chapter-2.py
@@ -2,37 +2,37 @@ first_name = "arnab"
 last_name="saha"
 full_name= first_name+""+last_name
 print(full_name)      #No Error
-# print(full_name+3)  // error  //becuse types are different here
+print(full_name+3)  # error  //becuse types are different here
 print(full_name+"3")      #No Error      
 print(full_name+str(3))      #No Error   //make 3 a str
 print(full_name*3)      #No Error       //multiplies name for 3 times
 
 
-# name=input("your name is : ")
-# print("your name is : " + name)   #we get inputs always in strings
+name=input("your name is : ")
+print("your name is : " + name)   #we get inputs always in strings
 
-# # number_1 = input("type your first number : ")
-# # number_2 = input("type your second number : ")
-# # sum_of_numbers= int(number_1)+int(number_2)
-# # print("total number is : "+str(sum_of_numbers))
-# # print(float(4))
+number_1 = input("type your first number : ")
+number_2 = input("type your second number : ")
+sum_of_numbers= int(number_1)+int(number_2)
+print("total number is : "+str(sum_of_numbers))
+print(float(4))
 
-# # name,age="arnab",20
-# # print("hey, "+name+" your age is : "+str(age)+" right")
-# # x=y=z=1
-# # print(x+y+z)
+name,age="arnab",20
+print("hey, "+name+" your age is : "+str(age)+" right")
+x=y=z=1
+print(x+y+z)
 
-# # name,age=input("enter your name and age (space separated) : ").split()
-# # print("hey, "+name+" your age is : "+str(age)+" right")
+name,age=input("enter your name and age (space separated) : ").split()
+print("hey, "+name+" your age is : "+str(age)+" right")
 
-# # name,age=input("enter your name and age (comma separated) : ").split(",")
-# # print("hey, "+name+" your age is : "+str(age)+" right")
-# # #string formating 
-# # #for python 3.6
-# # print(f"hello {name} your age is {age}")
+name,age=input("enter your name and age (comma separated) : ").split(",")
+print("hey, "+name+" your age is : "+str(age)+" right")
+#string formating 
+#for python 3.6
+print(f"hello {name} your age is {age}")
 
-# a,b,c=input("type any three numbers (comma separated): ").split(",")
-# print(f"avarage of these 3 numbers is : {(int(a)+int(b)+int(c))/3}")
+a,b,c=input("type any three numbers (comma separated): ").split(",")
+print(f"avarage of these 3 numbers is : {(int(a)+int(b)+int(c))/3}")
 
 lan = "python"
 print(lan[0]) #we start with 0 it we go left to right
@@ -64,9 +64,9 @@ print(name.rstrip()) #remove right spaces
 print(name.strip()) #remove left right both spaces
 
 
-# name,char=input("type your name and a letter (comma separated): ").split(",")
-# print(len(name.strip()))
-# print(name.lower().strip().count(char.strip()))
+name,char=input("type your name and a letter (comma separated): ").split(",")
+print(len(name.strip()))
+print(name.lower().strip().count(char.strip()))
 
 string="she is so beautiful, she is a good dancer too"
 print(string.replace(" ","_"))
--- chapter-3.py
+++ chapter-3.py
@@ -1,9 +1,23 @@
-import random
-winningNumber=random.randint(0,100)
-yourNumber=int(input("choose a number between 0-100: "))
-if winningNumber==yourNumber:
-    print("you win the game")
-elif winningNumber>yourNumber:
-    print("your number is too low!!!")
-elif winningNumber<yourNumber:
-    print("your number is too high!!!")
+# import random
+# winningNumber=random.randint(0,100)
+# print(winningNumber)
+# yourNumber=int(input("choose a number between 0-100: "))
+# if winningNumber==yourNumber:
+#     print("you win the game")
+# else:
+#     if winningNumber>yourNumber:
+#         print("your number is too low")
+#     if winningNumber<yourNumber:
+#         print("your number is too low")
+# # if winningNumber==yourNumber:
+# #     print("you win the game")
+# # elif winningNumber>yourNumber:
+# #     print("your number is too low!!!")
+# # elif winningNumber<yourNumber:
+# #     print("your number is too high!!!")
+
+# name,age= input("type your name and age (comma separated): ").split(",")
+# if name=="abc" or age=="20":
+#     print("good to go!!")
+# else:
+#     print("sorry no entry!!")
