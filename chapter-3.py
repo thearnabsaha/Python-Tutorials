f1a0de1f38226adff342f75b4f7495a42c2d2d0e
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-05 20:01:10 +0530

    s3

--- chapter-3.py
+++ chapter-3.py
@@ -1,1 +1,9 @@
-print("")
+import random
+winningNumber=random.randint(0,100)
+yourNumber=int(input("choose a number between 0-100: "))
+if winningNumber==yourNumber:
+    print("you win the game")
+elif winningNumber>yourNumber:
+    print("your number is too low!!!")
+elif winningNumber<yourNumber:
+    print("your number is too high!!!")
