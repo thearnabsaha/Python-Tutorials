fbad2901ab395bae062a36f21400052c96c4303b
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-07 11:16:39 +0530

    s4

--- chapter-4.py
+++ chapter-4.py
@@ -10,3 +10,29 @@
 #         return "your number is odd"
 # number=int(input("type any number: "))
 # print(isOddOrEven(number))
+
+
+# def isBig(a,b,c):
+#     if a>b and a>c:
+#         return a
+#     elif b>c and b>a:
+#         return b
+#     elif c>a and c>b:
+#         return c
+#     return b
+# num1=int(input("type a number: "))
+# num2=int(input("type a number: "))
+# num3=int(input("type a number: "))
+# print(isBig(num1,num2,num3))
+
+# def isBig(a,b):
+#     if a>b:
+#         return a
+#     return b
+
+# def isBigger(a,b,c):
+#     return isBig(isBig(a,b),c)
+# num1=int(input("type a number: "))
+# num2=int(input("type a number: "))
+# num3=int(input("type a number: "))
+# print(isBigger(num1,num2,num3))
