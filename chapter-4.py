a78ddb1079a073a2903a120fb974c08f5c1f1c46
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-07 13:18:23 +0530

    s4

--- chapter-4.py
+++ chapter-4.py
@@ -37,9 +37,49 @@
 # num3=int(input("type a number: "))
 # print(isBigger(num1,num2,num3))
 
-def ispalindrome(a):
-    if a[::-1]==a:
-        return "yes it is a palindrome"
-    return "no it's not a palindrome"
-string=input("type a palindrome: ")
-print(ispalindrome(string))
+# def ispalindrome(a):
+#     if a[::-1]==a:
+#         return "yes it is a palindrome"
+#     return "no it's not a palindrome"
+# string=input("type a palindrome: ")
+# print(ispalindrome(string))
+
+# def fib(n):
+#     a=0
+#     b=1
+#     if n<=0:
+#         print("invalid")
+#     elif n==1:
+#         print(a)
+#     elif n==2:
+#         print(a,b)
+#     else:
+#         print(a,b,end=" ")
+#         for i in range(n-2):
+#             c=a+b
+#             a=b
+#             b=c
+#             print(b,end=" ")
+# fib(20)
+
+# def bio(_name,_age=20):
+#     print(f"my name is {_name} and age is {_age}")
+# bio("arnab",40)
+
+def fib(n):
+    a=0
+    b=1
+    if n<=0:
+        print("invalid")
+    elif n==1:
+        print(a)
+    elif n==2:
+        print(a,b)
+    else:
+        print(a,b,end=" ")
+        for i in range(n-2):
+            c=a+b
+            a=b
+            b=c
+            print(b, end=" ")
+fib(10)
