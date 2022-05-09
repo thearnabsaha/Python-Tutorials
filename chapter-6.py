ebc3374b452935be682fb0380072994889a28e94
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-09 18:42:56 +0530

    s6

--- chapter-6.py
+++ chapter-6.py
@@ -1,2 +1,25 @@
-# days=("sunday","monday","tuesday",'wednesday',"thursday","friday","saturday")
-# print(days)
+days=("sunday","monday","tuesday",'wednesday',"thursday","friday","saturday")
+print(days)
+for i in days:
+    print(i)
+
+num=(1,)
+name=("arnab",)
+name=()
+print(type(name))
+
+days2="sunday","monday","tuesday",'wednesday',"thursday","friday","saturday"
+print(type(days2))
+
+name1,name2=("arnab","suborno")
+print(name2)
+
+listInTuples = ('arnab',['suborno',"sohini"])
+listInTuples[1].pop()
+listInTuples[1].append("ambitamta")
+print(listInTuples)
+
+nums=(1,2,3,4,5,6)
+print(min(nums))
+print(max(nums))
+print(sum(nums))
