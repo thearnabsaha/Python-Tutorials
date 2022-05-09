52d411df61eb93ac979b5877ea190969ed17d89f
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-09 19:48:20 +0530

    s7

---
+++ chapter-7.py
@@ -0,0 +1,11 @@
+user1={'name':"arnab","age":20}
+print(user1)
+user2=dict(name="suborno",age=20)
+print(user2)
+
+print(user1["name"])
+
+user3={}
+user3["name"]="ambitama"
+user3["age"]=21
+print(user3)
