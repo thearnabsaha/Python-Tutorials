c2ed2ebd2db11166ac64041b62452df9eecbd8af
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-09 11:50:17 +0530

    s5

--- chapter-5.py
+++ chapter-5.py
@@ -1,2 +1,12 @@
-listy=[12,"arnab",True,2.5,None]
+listy=[12,"arnab",True,2.5,None]  #we make a list with [] 
 print(listy)
+print(listy[0])  #to get any element from a list
+listy[0]="zero"  #it will change the element of zero position in this list
+listy[0:]="zero" #it will destructure the string in from of list
+listy[0:]=[1,3,5,6,8] #it will replace the array
+print(listy)
+fruits=[]
+fruits.append("apple")
+fruits.append("mango")
+fruits.append("grapes")
+print(fruits)
