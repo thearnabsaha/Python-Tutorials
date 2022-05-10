7ab564fd1f64f0c07d2c96997edcd99e163eb07e
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-10 21:16:18 +0530

    s12

---
+++ chapter-12.py
@@ -0,0 +1,12 @@
+add= lambda a,b:a+b
+print(add(2,3))
+
+mult= lambda a,b:a*b
+print(mult(2,3))
+
+isEven= lambda a:a%2==0
+print(isEven(2))
+
+func= lambda a: print("arnab") if a>2 else print("saha")
+
+func(1)
