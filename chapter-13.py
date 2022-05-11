3bf841d06805853bb5c9968b126660630f145e62
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-11 11:30:48 +0530

    s13

--- chapter-13.py
+++ chapter-13.py
@@ -10,14 +10,18 @@
 
 # print(func1(names,"ambitamas"))
 
-nums=[1,2,3,4,5]
-print(list(map(lambda a:a**2,nums)))
+# nums=[1,2,3,4,5]
+# print(list(map(lambda a:a**2,nums)))
 
-nums=[1,2,3,4,5]
-print(list(filter(lambda a:a%2==0,nums)))
+# nums=[1,2,3,4,5]
+# print(list(filter(lambda a:a%2==0,nums)))
 
-name=["arnab","ambitama","suborno","arijit","sayan"]
-nums=[1,2,3,4,5]
-print(list(zip(nums,name)))
-print(dict(zip(nums,name))) #cause we can change [(a,b),(c,d)] this kind of values to tuples but not the [(a,b,c),(d,e,f)] of value
+# name=["arnab","ambitama","suborno","arijit","sayan"]
+# nums=[1,2,3,4,5]
+# print(list(zip(nums,name)))
+# print(dict(zip(nums,name))) #cause we can change [(a,b),(c,d)] this kind of values to tuples but not the [(a,b,c),(d,e,f)] of value
 
+# zippedList=[(1,2),(3,4),(5,6),(7,8),(9,0)]
+# l1,l2=list(zip(*zippedList))
+# print(list(l1))
+# print(list(l2))
