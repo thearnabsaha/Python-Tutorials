eae34c4b4e223e5fc61a891e025e9cc65621e3a2
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-12 22:58:56 +0530

    s14

--- chapter-14.py
+++ chapter-14.py
@@ -1,68 +1,68 @@
-# def square(a):
-#     return a**2
-# s=square
-# print(s.__name__)
-# print(square.__name__)
-# print(s)
-# print(square)
-# print(square is s)
+def square(a):
+    return a**2
+s=square
+print(s.__name__)
+print(square.__name__)
+print(s)
+print(square)
+print(square is s)
 
-# def toPower(n):
-#     def calcPower(i):
-#         return i**n
-#     return calcPower
-# cube=toPower(3)
-# squr=toPower(2)
-# print(cube(4))
-# print(squr(4))
-# from functools import wraps
-# def decorator_function(any_function):
-#     @wraps(any_function)
-#     def wrapper_function(*args,**kwargs):
-#         print(f"you are calling {wrapper_function.__name__} function")
-#         print(wrapper_function.__doc__)
-#         return any_function(*args,**kwargs)
-#     return wrapper_function
+def toPower(n):
+    def calcPower(i):
+        return i**n
+    return calcPower
+cube=toPower(3)
+squr=toPower(2)
+print(cube(4))
+print(squr(4))
+from functools import wraps
+def decorator_function(any_function):
+    @wraps(any_function)
+    def wrapper_function(*args,**kwargs):
+        print(f"you are calling {wrapper_function.__name__} function")
+        print(wrapper_function.__doc__)
+        return any_function(*args,**kwargs)
+    return wrapper_function
 
-# @decorator_function
-# def func1(a,b):
-#     """the function would take 2 parameters and add them"""
-#     return a+b
-# print(func1(7,8))
-# import time
-# from functools import wraps
-# def decorator_function(any_function):
-#     @wraps(any_function)
-#     def wrapper_function(*args,**kwargs):
-#         t1=time.time()
-#         returned_value = any_function(*args,**kwargs)
-#         t2=time.time()
-#         print(t2-t1)
-#         return returned_value
-#     return wrapper_function
+@decorator_function
+def func1(a,b):
+    """the function would take 2 parameters and add them"""
+    return a+b
+print(func1(7,8))
+import time
+from functools import wraps
+def decorator_function(any_function):
+    @wraps(any_function)
+    def wrapper_function(*args,**kwargs):
+        t1=time.time()
+        returned_value = any_function(*args,**kwargs)
+        t2=time.time()
+        print(t2-t1)
+        return returned_value
+    return wrapper_function
 
-# @decorator_function
-# def func(a,b):
-#     return a+b
+@decorator_function
+def func(a,b):
+    return a+b
 
-# print(func(2,3))
+print(func(2,3))
 
-# from functools import wraps
-# def onlyDataAllowed(dataType):
-#     def onlyIntAllowed(function):
-#         wraps(function)
-#         def wrapper_function(*args,**kwargs):
-#             if all([type(i)==dataType for i in args]):
-#                 return function(*args,**kwargs)
-#             print("invalid argument")
-#         return wrapper_function
-#     return onlyIntAllowed
+from functools import wraps
+def onlyDataAllowed(dataType):
+    def onlyIntAllowed(function):
+        wraps(function)
+        def wrapper_function(*args,**kwargs):
+            if all([type(i)==dataType for i in args]):
+                return function(*args,**kwargs)
+            print("invalid argument")
+        return wrapper_function
+    return onlyIntAllowed
 
-# @onlyDataAllowed(str)
-# def addAll(*args):
-#     total=""
-#     for i in args:
-#         total+=i
-#     return total
-# print(addAll("a","c","b"))
+@onlyDataAllowed(str)
+def addAll(*args):
+    total=""
+    for i in args:
+        total+=i
+    return total
+print(addAll("a","c","b"))
 
