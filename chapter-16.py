7861c957b0468456a03af64e8005da51a9eceb8e
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-13 01:20:17 +0530

    s16

--- chapter-16.py
+++ chapter-16.py
@@ -1,11 +1,34 @@
+# class Person:
+#     def __init__(self,firstName,lastName,age):
+#         #instance Variables
+#         self.firstName=firstName
+#         self.lastName=lastName
+#         self.age=age
+
+# p1=Person("arnab","saha",20)
+# p2=Person("ambitama","saha",21)
+# print(p1.age)
+# print(p2.firstName+" "+p2.lastName)
+
+# class Laptop:
+#     def __init__(self,brand,name,price):
+#         self.brand=brand
+#         self.name=name
+#         self.price=price
+
+# l1=Laptop("acer","nitro",40000)
+# l2=Laptop("apple","macbook",130000)
+# print(l1.brand)
+# print(l2.name)
+
 class Person:
-    def __init__(self,firstName,lastName,age):
-        #instance Variables
-        self.firstName=firstName
-        self.lastName=lastName
-        self.age=age
-        
-p1=Person("arnab","saha",20)
-p2=Person("ambitama","saha",21)
-print(p1.age)
-print(p2.firstName+" "+p2.lastName)
+    def __init__(self,firstNaeme,LastName):
+        self.firstName=firstNaeme
+        self.lastName=LastName
+    def fullName(self):
+        print(f"your full name is {self.firstName} {self.lastName}")
+p1=Person("arnab","saha")
+p2=Person("ambitama","saha")
+print(p1.firstName)
+print(p2.lastName)
+p1.fullName()
