4f75ec9cff301b528cd1581129771eb9d605506e
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-14 11:59:28 +0530

    s18

---
+++ Chapter-18.py
@@ -0,0 +1,20 @@
+# f=open("file.txt") #to open the the file
+# print(f"cursor position: {f.tell()}") #to tell the position of the cursor
+# print(f.read()) #to read the file
+# print(f"cursor position: {f.tell()}")
+# f.seek(0) #to change cursor's position to 0
+# print(f"cursor position: {f.tell()}")
+# print(f.readline()) #will read only one line
+# f.seek(0) 
+# lines=f.readlines()
+# print(len(lines))
+# for i in lines:
+#     print(i)
+# print(f.name)
+# print(f.closed)
+# f.close() 
+
+# with open("file.txt") as f:
+#     data=f.read()
+#     print(data)
+
