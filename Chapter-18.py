9659349e4c4e85d859365bf4f6f1ffddd2d4fe4c
thearnabsaha <thearnabsaha201@gmail.com>
2022-05-14 12:34:47 +0530

    s18

--- Chapter-18.py
+++ Chapter-18.py
@@ -14,15 +14,34 @@
 # print(f.closed)
 # f.close() 
 
-with open("file.txt","w") as f:
-    f.write("i am a pokemon")
+# with open("file.txt","w") as f:
+#     f.write("i am a pokemon") #it overwites the actual content of the file.
     
-with open("file.txt","a") as f:
-    f.write("i am a pokemon yeash")
+# with open("file.txt","a") as f:
+#     f.write("i am a pokemon yeash") #it apprnds data at the end of the actual file.
 
-with open("file.txt","r+") as f:
-    f.seek(len(f.read()))
-    f.write("i am a pokemon yeash")
-    f.seek(0)
-    print(f.read())
+# with open("file.txt","r+") as f: #it can read and write data together
+#     f.seek(len(f.read()))
+#     f.write("i am a pokemon yeash")
+#     f.seek(0)
+#     print(f.read())
 
+# with open("file.txt") as rf:
+#     with open("file2.txt","w") as wf:
+#         wf.write(rf.read())
+
+# with open("file.txt") as rf:
+#     with open("file2.txt","w") as wf:
+#         for line in rf.readlines():
+#             name,age=line.split(",")
+#             wf.write(f"{name}:{age}")
+
+# with open("index.html","r") as rf:
+#     with open("file.txt","w") as wf:
+#         for line in rf.readlines():
+#             if "<a href=" in line:
+#                 pos=line.find("<a href=")
+#                 first_qoute= line.find('\"',pos)
+#                 second_qoute=line.find('\"',first_qoute+1)
+#                 url=line[first_qoute+1:second_qoute]
+#                 wf.write(url+"\n")
